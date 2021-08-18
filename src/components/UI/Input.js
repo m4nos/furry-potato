3031b1d1949e3603c210f43c2b5ba651664bcc33
m4nos <m4nos@users.noreply.github.com>
2021-08-18 19:00:51 +0300

    add input component

---
+++ src/components/UI/Input.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label>{props.label}</label>
+      <input {...props.input} />
+    </div>
+  );
+};
+
+export default Input;
