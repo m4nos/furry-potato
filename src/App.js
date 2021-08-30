8b75ca8e9ccb63047ddc35466d12c5c9bfa36bca
m4nos <m4nos@users.noreply.github.com>
2021-08-30 17:30:03 +0300

    import cart overlay state & handlers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsVisible, setCartIsVisible] = useState(false);
+
+  const viewCartHandler = () => {
+    setCartIsVisible(true);
+  };
+
+  const hideCardHandler = () => {
+    setCartIsVisible(false);
+  };
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsVisible && <Cart onClose={hideCardHandler} />}
+      <Header onViewCart={viewCartHandler} />
       <main>
         <Meals />
       </main>
