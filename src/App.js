302a6d0b83e827682db59b7324510a246bbb80bb
m4nos <m4nos@users.noreply.github.com>
2021-09-01 17:48:35 +0300

    replace Fragment with CartProvider to provide the cart context state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
@@ -14,13 +15,13 @@ function App() {
     setCartIsVisible(false);
   };
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsVisible && <Cart onClose={hideCardHandler} />}
       <Header onViewCart={viewCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
