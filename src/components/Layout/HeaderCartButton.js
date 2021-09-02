3748bc13e5aee3b72ca8283b472f070825eff449
m4nos <herman226.r@gmail.com>
2021-09-02 18:33:13 +0300

    import cart state as context to component

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,23 @@
-import React from "react";
-import CartIcon from "../Cart/CartIcon";
-import classes from "./HeaderCartButton.module.css";
+import React, { useContext } from 'react';
+import CartContext from '../../store/cart-context';
+import CartIcon from '../Cart/CartIcon';
+import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartContext = useContext(CartContext);
+
+  const numberOfCartItems = cartContext.items.reduce(
+    (sum, item) => sum + item.amount,
+    0
+  );
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
